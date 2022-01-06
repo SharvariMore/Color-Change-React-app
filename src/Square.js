7ac7ddaacdbed4c11f16e2fa1e4a0ad40a465dc8
Sharvari More <83008601+SharvariMore@users.noreply.github.com>
2022-01-07 00:33:09 +0530

    Changing the color in square as inputted

---
+++ src/Square.js
@@ -0,0 +1,19 @@
+const Sqaure = ({colorValue, hexValue, isDarkText}) => {
+    return ( 
+        <section className="square" 
+        style={{
+            backgroundColor: colorValue,
+            color: isDarkText ? "#000" : "#FFF"}}>
+        
+        <p>{colorValue ? colorValue : "Empty Value"}</p>
+        <p>{hexValue ? hexValue : null}</p>
+         
+        </section>
+     );
+}
+
+Sqaure.defaultProps = {
+    colorValue: "Empty Color Value"
+}
+ 
+export default Sqaure;
